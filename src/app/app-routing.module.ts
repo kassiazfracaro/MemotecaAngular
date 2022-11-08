eef3e69a2feab525f80ee2455664a03890ccfb39
kassiazama <kassiazama@gmail.com>
2022-11-07 21:21:33 -0300

    Crud em Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-pensamento',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listar-pensamento',
+    component: ListarPensamentosComponent
+  },
+  {
+    path: 'pensamentos/excluir-pensamento/:id',
+    component: ExcluirPensamentoComponent
+  },
+  {
+    path: 'pensamentos/editar-pensamento/:id',
+    component: EditarPensamentoComponent
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,22 @@
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit {
+
+listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
+    })
+  }
+
+}
